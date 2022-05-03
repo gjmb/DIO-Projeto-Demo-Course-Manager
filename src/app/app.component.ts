7ade0e57dd771bdc1180a0dcf424b462aac3e0c7
gjmb <gjmbarbosa@gmail.com>
2022-05-03 16:17:30 -0300

    Revert "Adiona o componente course-list e o framework bootstrap"
    This reverts commit fa410afa1e68565cf507f275ebdad51c372410af.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-//Decorator
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,5 +7,4 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'DIO-Projeto-Demo-Course-Manager';
-  name:string ='john Boy';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { CourseListComponent } from './courses/course-list.component';
-
 
 @NgModule({
   declarations: [
-    AppComponent,
-    CourseListComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    FormsModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
