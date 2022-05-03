fa410afa1e68565cf507f275ebdad51c372410af
gjmb <gjmbarbosa@gmail.com>
2022-05-03 16:11:36 -0300

    Adiona o componente course-list e o framework bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+//Decorator
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'DIO-Projeto-Demo-Course-Manager';
+  name:string ='john Boy';
 }
