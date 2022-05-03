b2cabdf6ed951633d024b8eb5b2419db1b358413
gjmb <gjmbarbosa@gmail.com>
2022-05-03 17:24:48 -0300

    Implementa o componente CourseListComponent
    Adiciona a classe course.ts e adiciona a tabela para exibir os dados
    usando o framework bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'DIO-Projeto-Demo-Course-Manager';
+  name = 'John Boy'
 }
--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -1,12 +1,18 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { Course } from "./course";
 
 @Component({
     selector:'app-course-list',
     templateUrl: './course-list.component.html'
 
 })
-export class CourseListComponent {
+export class CourseListComponent implements OnInit{
 
+    courses: Course [] = [];
 
+    ngOnInit(): void {
+        this.courses.push(new Course(1,'Angular: Forms','',99.99,'XPS-8796',120,0,'05/03/2022'));
+        this.courses.push(new Course(2,'Angular: http','',45.99,'LKL-1094',90,0,'05/03/2022'));
+    }
 
 }
