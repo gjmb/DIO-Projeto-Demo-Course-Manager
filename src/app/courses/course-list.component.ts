dd7d3c7feae56da3f943876675d7f19f20ec8cf9
gjmb <gjmbarbosa@gmail.com>
2022-05-05 15:22:16 -0300

    Adiciona um novo componente de rating usando stars
    Adiciona o toolkit Font Awesome para o sistema de rating
    Adiciona Imagens para os icones dos cursos

--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -3,16 +3,15 @@ import { Course } from "./course";
 
 @Component({
     selector:'app-course-list',
-    templateUrl: './course-list.component.html'
-
+    templateUrl: './course-list.component.html',
 })
 export class CourseListComponent implements OnInit{
 
     courses: Course [] = [];
 
     ngOnInit(): void {
-        this.courses.push(new Course(1,'Angular: Forms','',99.99,'XPS-8796',120,0,'05/03/2022'));
-        this.courses.push(new Course(2,'Angular: http','',45.99,'LKL-1094',90,0,'05/03/2022'));
+        this.courses.push(new Course(1,'Angular: Forms','/assets/images/forms.png',99.99,'XPS-8796',120,4.5,'05/03/2022'));
+        this.courses.push(new Course(2,'Angular: http','/assets/images/http.png',45.99,'LKL-1094',90,5,'05/03/2022'));
     }
 
 }
