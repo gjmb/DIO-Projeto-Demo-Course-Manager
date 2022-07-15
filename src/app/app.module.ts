ef1f92fadb31bb136aa84c6b14e3d1f5ff85fd2b
gjmb <gjmbarbosa@gmail.com>
2022-07-15 19:45:40 -0300

    Cria a pasta shared para agrupar componentes genéricos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 
 
 import { AppComponent } from './app.component';
-import { StarComponent } from './star/star.component';
-import { ReplacePipe } from './pipe/replace.pipe';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { Error404Component } from './error-404/error-404.component';
 import { CourseModule } from './courses/course.nodule';
